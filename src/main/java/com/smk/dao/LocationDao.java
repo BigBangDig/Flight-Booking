558d318a9976dbab965fd1c30a28710b03131ef4
artiz <artizarodion@gmail.com>
2023-11-23 11:10:40 +0700

    Still not finnish :( btw this is for another school stuff.

--- src/main/java/com/smk/dao/LocationDao.java
+++ src/main/java/com/smk/dao/LocationDao.java
@@ -3,7 +3,11 @@ package com.smk.dao;
 import com.smk.model.Location;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Optional;
 
 public class LocationDao implements Dao<Location, Integer>{
@@ -19,7 +23,26 @@ public class LocationDao implements Dao<Location, Integer>{
 
     @Override
     public Collection<Location> getAll() {
-        return null;
+        Collection<Location> result = new LinkedList<>();
+        String sql = "SELECT * from location";
+        connection.ifPresent(connection -> {
+            try {
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+
+                    Location location = new Location();
+                    location.setId(id);
+                    location.setName(name);
+                    result.add(location);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+        return result;
     }
 
     @Override
--- src/main/java/com/smk/view/CreateBooking.java
+++ src/main/java/com/smk/view/CreateBooking.java
@@ -1,6 +1,8 @@
 package com.smk.view;
 
 import com.smk.MainView;
+import com.smk.dao.LocationDao;
+import com.smk.model.Location;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -14,14 +16,22 @@ import java.awt.*;
 @PageTitle("Create Booking")
 @Route(value = "create-booking", layout = MainView.class)
 public class CreateBooking extends VerticalLayout {
+    private LocationDao locationDao;
     public CreateBooking() {
+        locationDao = new LocationDao();
         createForm();
     }
 
     private void createForm(){
         setAlignItems(Alignment.STRETCH);
-        ComboBox fromComboBox = new ComboBox("Dari");
-        ComboBox toCombobox = new ComboBox<>("ke");
+        ComboBox<Location> fromComboBox = new ComboBox<>("Dari");
+        fromComboBox.setItems(locationDao.getAll());
+        fromComboBox.setItemLabelGenerator(Location::getName);
+
+        ComboBox<Location> toCombobox = new ComboBox<>("ke");
+        fromComboBox.setItems(locationDao.getAll());
+        fromComboBox.setItemLabelGenerator(Location::getName);
+
         DatePicker departureDatePicker = new DatePicker("Tanggal keberangkatan");
         DatePicker arrivalDatePicker = new DatePicker("Tanggal kepulangan");
         Button searchButton =new Button("Search");
